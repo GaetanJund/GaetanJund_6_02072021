b505fb7e7911ecab3d7450511ec9bb8e692161d7
Gaetan Jund <gag67nba@gmail.com>
2021-07-19 09:38:51 +0200

    MAJ + Sécurité

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,10 +3,13 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 
+// Importe la vérification du password
+const verifPassword = require('../middleware/password-verif');
+
 // Créer 2 routes POST
 
-// Ajout de l'utilisateur dna sla base de donnée
-router.post('/signup', userCtrl.signup);
+// Ajout de l'utilisateur dans la base de donnée
+router.post('/signup', verifPassword, userCtrl.signup);
 // Connecte un utilisateur déjà inscrit
 router.post('/login', userCtrl.login);
 
